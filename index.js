1c1f4c0962b07d8b0aac2342a3ac755f039e7173
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-01-14 15:27:05 -0600

    Completed Post functionality for the API

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -42,10 +44,26 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
+const generateId = () => {
+  return Math.max(...notes.map((note) => note.id)) + 1;
+};
+
 app.post("/api/notes", (request, response) => {
-  const note = request.body;
-  console.log(note);
-  response.json(note);
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+  notes = notes.concat(note);
+  response.json(`Added this note: ${note}`);
+  console.log(`Added this note: ${note}`);
 });
 
 const PORT = 3001;
